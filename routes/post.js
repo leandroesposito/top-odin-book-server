974d09f11dce1a570b915a654495fa071f7ee72d
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-07-12 22:49:46 -0300

    Add comment routes and improve routes order

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,17 @@
 const controller = require("../controllers/post");
 const router = require("express").Router({ mergeParams: true });
 const likeRouter = require("./like");
+const commentRouter = require("./comment");
 
 router.get("/feed", controller.getFeedPosts);
-router.get("/:postId", controller.getPostById);
 router.get("/", controller.getAllPosts);
 router.post("/", controller.createPost);
-router.use("/:postId/like", likeRouter);
+
+router.get("/:postId", controller.getPostById);
 router.put("/:postId", controller.updatePost);
 router.delete("/:postId", controller.deletePost);
+
+router.use("/:postId/like", likeRouter);
+router.use("/:postId/comments", commentRouter);
 
 module.exports = router;
